0b31ba6517c316f5cf3253ae1df250c721fe0bf0
ZakuHuaso <g.vergara.val@gmail.com>
2024-11-14 23:40:33 -0300

    Initial commit

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
