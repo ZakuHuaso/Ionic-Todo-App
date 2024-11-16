afb9e4935ef6013bcd55948f4b41dc73a03edaab
ZakuHuaso <g.vergara.val@gmail.com>
2024-11-15 21:09:52 -0300

    feat Register page updated
    HTML and CSS from Register page updated, buttons added to register.page.ts and header component added in register.module.ts

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CancelAlertService } from 'src/app/service/custom-alert.service';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage implements OnInit {
 
-  constructor() { }
+export class RegisterPage {
 
-  ngOnInit() {
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private router: Router,
+    private alert: CancelAlertService
+  ) { }
+
+  onRegisterButton(){
+
   }
 
+  clean() {
+    this.email = '';
+    this.password = '';
+  }
 }
